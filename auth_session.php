97e32ce214b2149b301e06f5454fc012c4728f7f
webber017 <boukevanderveen@protonmail.com>
2022-05-19 09:35:04 +0200

    ''

---
+++ auth_session.php
@@ -0,0 +1,9 @@
+<?php
+    //Is nodig om de variables te krijgen van de vorige session
+    session_start();
+    //Als de $_SESSION variable niet geinitializeerd is, dan gaat hij naar login.php. Anders continueerd de code.
+    if(!isset($_SESSION["username"])) {
+        header("Location: login.php");
+        exit();
+    }
+?>
